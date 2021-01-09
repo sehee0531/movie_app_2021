8960b2d579ae167df6a12b4c8aa0ff7bb4aef656
sehee0531 <hello.git.GitHub@gmail.com>
2021-01-09 21:50:00 +0900

    map practice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import Banana from "./Banana";
+
+function Food({name,fav}){
+  return(
+  <div>
+    <h3>I Like {name}</h3>
+    <img src={fav} />
+  </div>
+  );
+}
+
+const foodILike = [
+  {
+    name: "rabokki",
+    image:
+    "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name:"smagiupsal",
+    image:
+    "https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA4MjZfMTQz%2FMDAxNTk4NDQ5MTAwMzkz.FXCKPDduXF8WHaiPwBphfLWU1PBdsbGFbQUcMjEABsMg.JFXKgR1mI5uuHu0UJaRZr3NmtgoAIRyB2LyeisNgu5og.JPEG.rinvely0322%2F%25C3%25B5%25C8%25A3%25B0%25ED%25B1%25E2%25C1%25FD_%25BB%25EF%25B0%25E3%25BB%25EC_%252826%2529.JPG&type=sc960_832"
+  }
+];
 
 function App() {
   return (
   <div>
-    <h1>Hello!!!My name is SeHee</h1>
-    <Banana />
+    {foodILike.map(dish=> (
+    <Food name={dish.name} fav={dish.image} /> 
+    ))}
   </div>
   );
 }
--- src/Banana.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Banana(){
-    return <h3>I Love Banana</h3>;
-}
-
-export default Banana;
